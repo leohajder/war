d981cd9ac0b6d87a8da9526e22732c6b56b6eed5
Leo Hajder <leohajder@gmail.com>
2019-03-21 20:21:16 +0100

    Get army counts from request query

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends AbstractController
 {
     /**
      * Displays app index
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        /** @var int $army1 */
+        $army1 = (int) $request->query->getInt('army1');
+        /** @var int $army2 */
+        $army2 = (int) $request->query->getInt('army2');
+
+        return $this->render('default/index.html.twig', [
+            'army1' => $army1,
+            'army2' => $army2,
+        ]);
     }
 }
