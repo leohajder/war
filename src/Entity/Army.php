d78c53b51d3450709f564592d92b7deeac799f0d
Leo Hajder <leohajder@gmail.com>
2019-03-24 11:50:01 +0100

    Generate entity

---
+++ src/Entity/Army.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+class Army
+{
+    private $id;
+
+    private $tag;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTag(): ?string
+    {
+        return $this->tag;
+    }
+
+    public function setTag(string $tag): self
+    {
+        $this->tag = $tag;
+
+        return $this;
+    }
+}
