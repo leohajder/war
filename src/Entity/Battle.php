cc18c1618b5ca3909fdb2f687e3ed156e46bca7f
Leo Hajder <leohajder@gmail.com>
2019-03-24 11:40:18 +0100

    Generate entities

--- src/Entity/Battle.php
+++ src/Entity/Battle.php
@@ -6,8 +6,22 @@ class Battle
 {
     private $id;
 
+    private $war;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getWar(): ?War
+    {
+        return $this->war;
+    }
+
+    public function setWar(?War $war): self
+    {
+        $this->war = $war;
+
+        return $this;
     }
 }
--- src/Entity/War.php
+++ src/Entity/War.php
@@ -2,12 +2,53 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 class War
 {
     private $id;
 
+    private $battles;
+
+    public function __construct()
+    {
+        $this->battles = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection|Battle[]
+     */
+    public function getBattles(): Collection
+    {
+        return $this->battles;
+    }
+
+    public function addBattle(Battle $battle): self
+    {
+        if (!$this->battles->contains($battle)) {
+            $this->battles[] = $battle;
+            $battle->setWar($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBattle(Battle $battle): self
+    {
+        if ($this->battles->contains($battle)) {
+            $this->battles->removeElement($battle);
+            // set the owning side to null (unless already changed)
+            if ($battle->getWar() === $this) {
+                $battle->setWar(null);
+            }
+        }
+
+        return $this;
     }
 }
